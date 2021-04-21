922203c9f7c791267a3d9e0fe6bf876dfa22795e
sharad <sharad.rajore@gmail.com>
2021-04-21 15:55:29 +0530

    create war for spring boot applicstion

--- spring-starter-project/src/main/java/com/zensar/entities/User.java
+++ spring-starter-project/src/main/java/com/zensar/entities/User.java
@@ -1,6 +1,8 @@
 package com.zensar.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.ToString;
 public class User {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int userId;
 	
 	private String userName;
