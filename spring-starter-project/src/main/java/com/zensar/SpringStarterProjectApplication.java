2fd705c45f41af364357f170f3042412b56147b6
sharad <sharad.rajore@gmail.com>
2021-04-21 17:53:25 +0530

    change port number programatically

--- spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
@@ -1,5 +1,7 @@
 package com.zensar;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,7 +11,22 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class SpringStarterProjectApplication  {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringStarterProjectApplication.class, args);
+		//SpringApplication.run(SpringStarterProjectApplication.class, args);
+		
+		 SpringApplication app = new SpringApplication(SpringStarterProjectApplication.class);
+	        app.setDefaultProperties(Collections.singletonMap("server.port", "9001"));
+	        app.run(args);
+		
+		 
+		 
+		/*
+		 * SpringApplication springApplication = new
+		 * SpringApplicationBuilder(SpringStarterProjectApplication.class)
+		 * .properties("spring.config.name=myapplication") .build();
+		 * 
+		 * springApplication.setDefaultProperties(Collections.singletonMap(
+		 * "server.port", "6666")); springApplication.run(args);
+		 */
 	}
 
 	
