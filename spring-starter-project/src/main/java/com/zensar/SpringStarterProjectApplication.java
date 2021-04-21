3d1b8a0474e3b72489fc931aa568ea60577cc166
sharad <sharad.rajore@gmail.com>
2021-04-21 17:55:07 +0530

    change name of application.properties file

--- spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
@@ -1,7 +1,5 @@
 package com.zensar;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,20 +11,10 @@ public class SpringStarterProjectApplication  {
 	public static void main(String[] args) {
 		//SpringApplication.run(SpringStarterProjectApplication.class, args);
 		
-		 SpringApplication app = new SpringApplication(SpringStarterProjectApplication.class);
-	        app.setDefaultProperties(Collections.singletonMap("server.port", "9001"));
-	        app.run(args);
-		
-		 
-		 
-		/*
-		 * SpringApplication springApplication = new
-		 * SpringApplicationBuilder(SpringStarterProjectApplication.class)
-		 * .properties("spring.config.name=myapplication") .build();
-		 * 
-		 * springApplication.setDefaultProperties(Collections.singletonMap(
-		 * "server.port", "6666")); springApplication.run(args);
-		 */
+		SpringApplication springApplication = new SpringApplicationBuilder(SpringStarterProjectApplication.class)
+		.properties("spring.config.name=myapplication")
+		.build();
+		springApplication.run(args);
 	}
 
 	
