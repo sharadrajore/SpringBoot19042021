6e442b0aaa6f7321af243d9db19ae35ea0a779ff
sharad <sharad.rajore@gmail.com>
2021-04-22 16:06:27 +0530

    demo of CommandLineRunner interface

--- spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
@@ -1,31 +1,28 @@
 package com.zensar;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 import com.zensar.controllers.UserController;
 import com.zensar.entities.User;
 
 @SpringBootApplication
-public class SpringStarterProjectApplication  {
+public class SpringStarterProjectApplication  implements CommandLineRunner  {
+	
+	@Autowired
+	private   UserController controller;
 
 	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		
-		ConfigurableApplicationContext context = SpringApplication.run(SpringStarterProjectApplication.class, args);
-		
-		// UserController bean = (UserController)context.getBean("userController");
-		UserController bean = context.getBean("user",UserController.class);
-		User user1 = new User(1,"test",20,"test@gmail.com");
-		User user2 = new User(2,"user",30,"user@gmail.com");
-		bean.insertUser(user1);
-		bean.insertUser(user2);
-		/*
-		 * SpringApplication springApplication = new
-		 * SpringApplicationBuilder(SpringStarterProjectApplication.class)
-		 * .properties("spring.config.name=myapplication") .build();
-		 * springApplication.run(args);
-		 */
+		User user = new User(1,"test",12,"test@test.com");
+		controller.insertUser(user);
 	}
 
 	
