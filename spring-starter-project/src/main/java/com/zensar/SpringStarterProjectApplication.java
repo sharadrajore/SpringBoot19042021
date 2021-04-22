59d93b67f6f68e9c60506eb09c63aa2bda1a42ce
sharad <sharad.rajore@gmail.com>
2021-04-22 16:51:07 +0530

    change default embeded container to jetty

--- spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
@@ -1,34 +1,15 @@
 package com.zensar;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.zensar.controllers.UserController;
-import com.zensar.entities.User;
-
 @SpringBootApplication
-public class SpringStarterProjectApplication  implements ApplicationRunner  {
-	
-	@Autowired
-	private   UserController controller;
+public class SpringStarterProjectApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStarterProjectApplication.class, args);
 	}
-
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		User user = new User(1,"user",12,"user@test.com");
-		controller.insertUser(user);
-		
-	}
-
-	
-	
 
 }
---
+++ spring-starter-project/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+insert  into user(user_age,user_email,user_name,user_id)values(40,'sharad.rajore@gmail.com','sharad',1)
